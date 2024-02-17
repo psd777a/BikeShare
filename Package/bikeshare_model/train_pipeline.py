2c148114478d763ba8128c8f8726608c5340839e
Srivatsa Datta <63804353+psd777a@users.noreply.github.com>
2024-02-17 10:09:13 +0530

    Chnages

--- Package/bikeshare_model/train_pipeline.py
+++ Package/bikeshare_model/train_pipeline.py
@@ -8,7 +8,6 @@ sys.path.append(str(root))
 import warnings
 
 warnings.filterwarnings("ignore")
-import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from bikeshare_model.config.core import config
